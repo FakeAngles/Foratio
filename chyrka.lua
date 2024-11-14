103a992a5110c5f7b616b7a79089fcff3855a1b2
FakeAngles <70075899+FakeAngles@users.noreply.github.com>
2024-11-14 08:33:03 +0300

    Update chyrka.lua

--- chyrka.lua
+++ chyrka.lua
@@ -630,7 +630,7 @@ function Menu:EnableHELL()
 		-- Modify DamageMult and OverheatMult
 		local damageMult = config:FindFirstChild("DamageMult")
 		if damageMult then
-			damageMult.Value = 10
+			damageMult.Value = 5
 		end
 
 		local overheatMult = config:FindFirstChild("OverheatMult")
@@ -656,7 +656,7 @@ function Menu:EnableHELL()
 
 			local explosiveMult = shellType:FindFirstChild("ExplosiveMult")
 			if explosiveMult then
-				explosiveMult.Value = 5000
+				explosiveMult.Value = 250
 			end
 
 			local penetration = shellType:FindFirstChild("Penetration")
