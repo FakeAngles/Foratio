7c67c7673d11135e1b1966b7e28f6b04a018da91
FakeAngles <70075899+FakeAngles@users.noreply.github.com>
2024-12-28 10:34:47 +0300

    Update SecretProject.lua

--- SecretProject.lua
+++ SecretProject.lua
@@ -1,6 +1,3 @@
-
-
-
 if not game:IsLoaded() then 
     game.Loaded:Wait()
 end
@@ -9,57 +6,64 @@ if not syn or not protectgui then
     getgenv().protectgui = function() end
 end
 
-setthreadidentity(2)
+if bypass_adonis then
+    task.spawn(function()
+        local g = getinfo or debug.getinfo
+        local d = false
+        local h = {}
 
-for i, v in getgc(true) do
-    if typeof(v) == "table" then
-        local a = rawget(v, "Detected")
-        local b = rawget(v, "Kill")
-    
-        if typeof(a) == "function" and not x then
-            x = a
+        local x, y
+
+        setthreadidentity(2)
+
+        for i, v in getgc(true) do
+            if typeof(v) == "table" then
+                local a = rawget(v, "Detected")
+                local b = rawget(v, "Kill")
             
-            local o; o = hookfunction(x, function(c, f, n)
-                if c ~= "_" then
-                    if d then
-                        warn(`Adonis AntiCheat flagged\nMethod: {c}\nInfo: {f}`)
-                    end
+                if typeof(a) == "function" and not x then
+                    x = a
+                    local o; o = hookfunction(x, function(c, f, n)
+                        if c ~= "_" then
+                            if d then
+                                warn(`Adonis AntiCheat flagged\nMethod: {c}\nInfo: {f}`)
+                            end
+                        end
+                        
+                        return true
+                    end)
+                    table.insert(h, x)
                 end
-                
-                return true
-            end)
 
-            table.insert(h, x)
+                if rawget(v, "Variables") and rawget(v, "Process") and typeof(b) == "function" and not y then
+                    y = b
+                    local o; o = hookfunction(y, function(f)
+                        if d then
+                            warn(`Adonis AntiCheat tried to kill (fallback): {f}`)
+                        end
+                    end)
+                    table.insert(h, y)
+                end
+            end
         end
 
-        if rawget(v, "Variables") and rawget(v, "Process") and typeof(b) == "function" and not y then
-            y = b
-            local o; o = hookfunction(y, function(f)
+        local o; o = hookfunction(getrenv().debug.info, newcclosure(function(...)
+            local a, f = ...
+
+            if x and a == x then
                 if d then
-                    warn(`Adonis AntiCheat tried to kill (fallback): {f}`)
+                    warn(`zins | adonis bypassed`)
                 end
-            end)
 
-            table.insert(h, y)
-        end
-    end
+                return coroutine.yield(coroutine.running())
+            end
+            
+            return o(...)
+        end))
+
+        setthreadidentity(7)
+    end)
 end
-
-local o; o = hookfunction(getrenv().debug.info, newcclosure(function(...)
-    local a, f = ...
-
-    if x and a == x then
-        if d then
-            warn(`PasteWare | adonis bypassed`)
-        end
-
-        return coroutine.yield(coroutine.running())
-    end
-    
-    return o(...)
-end))
-
-setthreadidentity(7)
 
 local SilentAimSettings = {
     Enabled = false,
@@ -537,7 +541,6 @@ RunService.RenderStepped:Connect(function()
         end
     end
 end)
-
 
 aimbox:AddToggle("antiLock_Enabled", {
     Text = "Enable Anti Lock Resolver",
