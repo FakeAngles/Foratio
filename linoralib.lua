8d8e3bd895bd218bbddaaa443a6b6439de54ec42
FakeAngles <70075899+FakeAngles@users.noreply.github.com>
2024-12-31 14:18:42 +0300

    Update linoralib.lua

--- linoralib.lua
+++ linoralib.lua
@@ -3603,20 +3603,19 @@ function Library:CreateWindow(...)
     end
 
     Library:GiveSignal(InputService.InputBegan:Connect(function(Input, Processed)
-        if Library.ToggleKeybindEnabled then
-            if Input.UserInputType == Enum.UserInputType.Keyboard and Input.KeyCode == Library.ToggleKeybind then
+        if type(Library.ToggleKeybind) == 'table' and Library.ToggleKeybind.Type == 'KeyPicker' then
+            if Input.UserInputType == Enum.UserInputType.Keyboard and Input.KeyCode.Name == Library.ToggleKeybind.Value then
                 task.spawn(Library.Toggle)
             end
         elseif Input.KeyCode == Enum.KeyCode.RightControl or (Input.KeyCode == Enum.KeyCode.RightShift and (not Processed)) then
             task.spawn(Library.Toggle)
         end
     end))
-    
-    if Config.AutoShow then
-        task.spawn(Library.Toggle)
-    end
-    
-    Window.Holder = Outer
+
+    if Config.AutoShow then task.spawn(Library.Toggle) end
+
+    Window.Holder = Outer;
+
     return Window;
 end;
 
